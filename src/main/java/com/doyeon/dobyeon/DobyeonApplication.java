feda791de3cf5317393991f3b87cd0297ae747e8
Minjun Lee <127070837+MinjuN07@users.noreply.github.com>
2024-12-23 13:55:20 +0900

    Initial commit

---
+++ src/main/java/com/doyeon/dobyeon/DobyeonApplication.java
@@ -0,0 +1,13 @@
+package com.doyeon.dobyeon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DobyeonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DobyeonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/doyeon/dobyeon/DobyeonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doyeon.dobyeon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DobyeonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
